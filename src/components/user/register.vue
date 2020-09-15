ef8a05a1c9abce62193f2ccea7fd3a7dd5b50638
909270950qq.com <909270950qq.com>
2020-09-15 08:48:52 +0800

    注册

--- src/components/user/register.vue
+++ src/components/user/register.vue
@@ -8,7 +8,12 @@
             </el-input>
             <el-input autocomplete="off" v-model="params.smsCode" placeholder="请输入验证码" class="mt-3">
                 <i slot="prefix" class="el-icon-chat-dot-square el-input__icon"></i>
-                <el-button slot="append" @click="getCode">获取验证码</el-button>
+                <el-button slot="append" @click="getCode" :disabled="time>0" loading-text="加载中">
+                    <span v-if="time===0">获取验证码</span>
+                    <span v-else>
+                        <span class="text-primary">{{time}}s</span>
+                    </span>
+                </el-button>
             </el-input>
             <el-input autocomplete="off" v-model="params.password" type="password" placeholder="请输入您的新密码" clearable
                       class="mt-3">
@@ -41,13 +46,14 @@
             ElForm: Form,
             ElPageHeader: PageHeader
         },
-        computed:{
+        computed: {
             ...mapState({
                 userFlag: state => state.user.userFlag,
             }),
         },
         data() {
             return {
+                time: 0,
                 params: {
                     mobile: '',
                     smsCode: '',
@@ -61,29 +67,59 @@
                 this.$store.commit('userFlag', -1)
             },
             login() {
-                this.requestPost('user/register', this.params, () => {
-                    this.$store.commit('showMessage', {
-                        type: 'success',
-                        message: '注册成功'
+                if (this.userFlag === -2) {
+                    this.requestPost('user/register', this.params, () => {
+                        this.$store.commit('showMessage', {
+                            type: 'success',
+                            message: '注册成功'
+                        })
+                        this.$store.commit('userFlag', -1)
+                    }, error => {
+                        this.$store.commit('showMessage', {
+                            message: '注册失败：' + error.message,
+                            type: 'error'
+                        })
                     })
-                    this.$store.commit('userFlag', -1)
-                }, error => {
-                    this.$store.commit('showMessage', {
-                        message: '注册失败：' + error.message,
-                        type: 'error'
+                } else if (this.userFlag === -3) {
+                    this.requestPost('user/resetPwd', this.params, () => {
+                        this.$store.commit('showMessage', {
+                            type: 'success',
+                            message: '重置成功'
+                        })
+                        this.$store.commit('userFlag', -1)
+                    }, error => {
+                        this.$store.commit('showMessage', {
+                            message: '重置失败：' + error.message,
+                            type: 'error'
+                        })
                     })
-                })
+                }
+
             },
             getCode() {
-                // if (this.params.mobile && userFlag === -2) {
-                //     this.requestPost('user/sendSms', this.params, res => {
-                //         console.log(res)
-                //     }, error => {
-                //         console.log(error)
-                //     })
-                // }else if(this.params.mobile && userFlag === -3){
-                //
-                // }
+                if (this.params.mobile && this.userFlag === -2) {
+                    this.requestPost('user/sendSms', this.params, res => {
+                        console.log(res)
+                        this.time = 60
+                        this.timer = setTimeout(()=> {
+                            this.time--
+                            if (this.time > 0) {
+                                this.timer
+                                console.log(4455)
+                            } else {
+                                clearTimeout(this.timer)
+                            }
+                        }, 1000)
+                    }, error => {
+                        console.log(error)
+                    })
+                } else if (this.params.mobile && this.userFlag === -3) {
+                    this.requestPost('user/sendSmsReset', this.params, res => {
+                        console.log(res)
+                    }, error => {
+                        console.log(error)
+                    })
+                }
             }
         }
     }
