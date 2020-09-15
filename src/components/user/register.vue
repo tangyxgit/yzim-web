686af547c9a8be45c47d60318ac8a0d84ad354f0
909270950qq.com <909270950qq.com>
2020-09-15 22:56:22 +0800

    注册登录

--- src/components/user/register.vue
+++ src/components/user/register.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="wrapper">
-        <el-page-header @back="backLogin" class="top w-100" content="">
-        </el-page-header>
-        <div class="center">
+        <div class="w-100 pl-2 pt-2"><span class="el-icon-arrow-left text-primary"></span><span @click="backLogin" class="text-primary">返回</span></div>
+        <div class="center ">
             <el-form label-position="left">
                 <el-input autocomplete="off" v-model="params.mobile" placeholder="请输入手机号" clearable>
                     <i slot="prefix" class="el-icon-user el-input__icon"></i>
