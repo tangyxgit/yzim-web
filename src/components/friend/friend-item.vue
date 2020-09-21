ba257b72d66d362a88dea1bc3cb52d995ed81939
909270950qq.com <909270950qq.com>
2020-09-21 22:05:02 +0800

    好友list  UI

--- src/components/friend/friend-item.vue
+++ src/components/friend/friend-item.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <el-row class="friend-item-container">
-      <el-col :span="6"  class="bg-primary pl-4">
+    <el-row class="friend-item-container" style="height:48px">
+      <el-col :span="6"  class="pl-4 pt-2 pb-2">
         <avatar :src="friend.profile.avatar" style="width: 32px;height: 32px;border-radius: 5px"/>
       </el-col>
-      <el-col :span="18">
-        <div class="friend-name bg-success row " style="height: 32px;">{{friend.profile.nick||friend.userID}}</div>
+      <el-col :span="18" >
+        <el-row style="height: 48px;" type="flex" align="middle" class="pl-1">
+          <el-col style="font-size: 14px;color: white">{{friend.profile.nick||friend.userID}}</el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
